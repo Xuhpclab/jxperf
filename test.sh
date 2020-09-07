8cf52f80f5dcab3ae1b881b7c529ef366a60e1a1
bolunli11 <bli11@email.wm.com>
2020-09-07 16:37:50 -0400

    script

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 CUR_DIR=$(cd "$(dirname "$0")";pwd)
 
-export LD_PRELOAD=${CUR_DIR}/build/preload/libpreload.so 
+export LD_PRELOAD=${CUR_DIR}/build/preload/libpreload.so
 java \
-    -javaagent:${CUR_DIR}/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar \
+    -javaagent:$JAVA_AGENT \
     -agentpath:${CUR_DIR}/build/libagent.so=DataCentric::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@100000 -jar \
     ${CUR_DIR}/benchmark/dacapo.jar lusearch
 
-${CUR_DIR}/script/process_raw_data.py
+#${CUR_DIR}/script/process_raw_data.py
