bcbdbafb66bd12f3b374ce80de5ab5d26ed83dc8
bolunli11 <bli11@email.wm.com>
2020-09-07 16:48:06 -0400

    script

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 CUR_DIR=$(cd "$(dirname "$0")";pwd)
+JVMTI_AGENT="$CUR_DIR"/build/libagent.so=DataCentric::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@100000
+BENCHMARK_DIR="$CUR_DIR"/benchmark/dacapo.jar
+BENCHMARK=lusearch
 
-export LD_PRELOAD=${CUR_DIR}/build/preload/libpreload.so
+export LD_PRELOAD="$CUR_DIR"/build/preload/libpreload.so
 java \
     -javaagent:$JAVA_AGENT \
-    -agentpath:${CUR_DIR}/build/libagent.so=DataCentric::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@100000 -jar \
-    ${CUR_DIR}/benchmark/dacapo.jar lusearch
-
-#${CUR_DIR}/script/process_raw_data.py
+    -agentpath:"$JVMTI_AGENT" \
+    -jar \
+    "$BENCHMARK_DIR" \
+    "$BENCHMARK"
