4ae2f4d58b311ef166528a317a7718b2744befaa
bolunli11 <bli11@email.wm.com>
2020-09-07 14:47:08 -0400

    fix script

--- run_attach.sh
+++ run_attach.sh
@@ -4,11 +4,14 @@ ATTACH=$JXPerf_HOME/bin/jattach/attach
 MODE=DataCentric::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@1000000
 PID="$1"
 DURATION="$2"
+ATTACH_TO_JAVA_AGENT=load instrument false $JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar
+ATTACH_TO_JVMTI_AGENT_START=load $JXPerf_HOME/build/libagent.so true
+ATTACH_TO_JVMTI_AGENT_STOP=load $JXPerf_HOME/build/libstop.so true
 
-"$ATTACH" "$PID" load instrument false $JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar
-"$ATTACH" "$PID" load $JXPerf_HOME/build/libagent.so true "$MODE"s
-while (( DURATION-- > 0 )) 
+"$ATTACH" "$PID" "$ATTACH_TO_JAVA_AGENT"
+"$ATTACH" "$PID" "$ATTACH_TO_JVMTI_AGENT_START" "$MODE"s
+while (( DURATION-- > 0 ))
 do
     sleep 1
 done
-"$ATTACH" "$PID" load $JXPerf_HOME/build/libstop.so true "$MODE"p
+"$ATTACH" "$PID" "$ATTACH_TO_JVMTI_AGENT_STOP" "$MODE"p
