c6062df5324be856f4ad6e8d0ab2a4c424f6d065
bolunli11 <bli11@email.wm.com>
2020-09-11 20:07:51 -0400

    make

--- .github/action_scripts/make.sh
+++ .github/action_scripts/make.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-DIR=$(cd "$(dirname "$0")";pwd)/../..
+DIR=$(cd "$(dirname "$0")";pwd)
 
 git submodule update --init --recursive
-cd $(DIR)/thirdparty/watchpoint-lib && make install PREFIX=$(CURRENT_DIR)/build/thirdparty
+cd $DIR/../../thirdparty/watchpoint-lib
