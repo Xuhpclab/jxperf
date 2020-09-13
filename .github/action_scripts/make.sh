35c4a739ae24b92294dac3f097ad5fe3440d50e3
bolunli11 <bli11@email.wm.com>
2020-09-13 13:59:30 -0400

    make.sh

--- .github/action_scripts/make.sh
+++ .github/action_scripts/make.sh
@@ -6,4 +6,5 @@ git submodule update --init --recursive
 cd $DIR/../../thirdparty/watchpoint-lib && make install PREFIX=$DIR/../../build/thirdparty
 cd $DIR/../../thirdparty/xed && ./mfile.py --debug --shared --prefix=$DIR/../../build/thirdparty install
 cd $DIR/../../thirdparty/libpfm-4.10.1 &&  make PREFIX=$DIR/../../build/thirdparty install
-cd $DIR/../../thirdparty/boost && sh ./bootstrap.sh --prefix=$DIR/../../build/thirdparty --with-libraries="filesystem"  cxxflags="-std=c++11" && ./b2 -j 4 && ./b2 filesystem install 
+cd $DIR/../../thirdparty/boost && sh ./bootstrap.sh --prefix=$DIR/../../build/thirdparty --with-libraries="filesystem"  cxxflags="-std=c++11" && ./b2 -j 4 && ./b2 filesystem install
+cd $DIR/../../thirdparty/bintrees &&  python setup.py install --user
