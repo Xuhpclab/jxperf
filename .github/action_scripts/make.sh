775cf7e2a8679ef3a30ad836ea30d20695fe125b
bolunli11 <bli11@email.wm.com>
2020-09-13 14:11:09 -0400

    make.sh

--- .github/action_scripts/make.sh
+++ .github/action_scripts/make.sh
@@ -9,3 +9,5 @@ cd $DIR/../../thirdparty/libpfm-4.10.1 &&  make PREFIX=$DIR/../../build/thirdpar
 cd $DIR/../../thirdparty/boost && sh ./bootstrap.sh --prefix=$DIR/../../build/thirdparty --with-libraries="filesystem"  cxxflags="-std=c++11" && ./b2 -j 4 && ./b2 filesystem install
 cd $DIR/../../thirdparty/bintrees &&  python setup.py install --user
 cd $DIR/../../thirdparty/allocation-instrumenter && mvn package -DskipTests
+mkdir $DIR/../../build/preload
+cd $DIR/../../preload && make
