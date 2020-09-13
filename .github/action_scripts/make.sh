5c03be6061a5d35e9bf3cd689c13c06a355522bd
bolunli11 <bli11@email.wm.com>
2020-09-13 12:58:04 -0400

    yml

--- .github/action_scripts/make.sh
+++ .github/action_scripts/make.sh
@@ -4,3 +4,4 @@ DIR=$(cd "$(dirname "$0")";pwd)
 
 git submodule update --init --recursive
 cd $DIR/../../thirdparty/watchpoint-lib && make install PREFIX=$DIR/../../build/thirdparty
+cd $DIR/../../thirdparty/xed && ./mfile.py --debug --shared --prefix=$DIR/../../build/thirdparty install
