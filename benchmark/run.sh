6c7f3e6d96aafab457dd652f9e9923b1fff3a8f9
bolunli11 <bli35@ncsu.edu>
2021-09-14 13:14:11 -0400

    update bench

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -2,7 +2,10 @@
 LD_PRELOAD=$JXPerf_HOME/build/libpreload.so
 java \
     -agentpath:$JXPerf_HOME/build/libagent.so=Generic::CYCLES:precise=2@100000 \
-    -cp ./test-IrisClassifier.jar org.deeplearning4j.examples.quickstart.modeling.feedforward.classification.IrisClassifier
+    -jar ./dacapo.jar lusearch
 
 # Offline process -- VS Code GUI
-# $JXPerf_HOME/script/process_raw_data_to_vscode.py /home/bli35/benchmarks/deeplearning4j
+# $JXPerf_HOME/script/process_raw_data_to_vscode.py ./lucene
+
+# View
+# code ./lucene
