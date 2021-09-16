30d21884316f565ef49439b355be6a9ad9e0c8f4
bolunli11 <bli35@ncsu.edu>
2021-09-15 20:39:41 -0400

    turn on wp on eb

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -4,6 +4,10 @@
 #     -agentpath:$JXPerf_HOME/build/libagent.so=Generic::CYCLES:precise=2@100000 \
 #     -jar ./dacapo.jar lusearch
 
+# dead store
+LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
+java -agentpath:$JXPerf_HOME/build/libagent.so=SilentStore::MEM_UOPS_RETIRED:ALL_STORES:precise=2@100000 -jar ./dacapo.jar lusearch
+
 # Offline process -- VS Code GUI
 # $JXPerf_HOME/script/process_raw_data_to_vscode.py /home/bli35/jxperf/benchmark/lucene
 
