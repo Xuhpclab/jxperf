60a0167065049f527e69201978d7be026dca97af
bolunli11 <bli35@ncsu.edu>
2021-09-27 13:51:32 -0400

    minor

--- bin/wallclock/runWallClock.sh
+++ bin/wallclock/runWallClock.sh
@@ -1,1 +1,5 @@
-java -agentpath:$JXPerf_HOME/bin/wallclock/wallClockProfile.so=start,event=wall,interval=5ms,file=profile.html -jar $JXPerf_HOME/benchmark/dacapo.jar -s large lusearch
+JAVA=java
+INSTRUMENT=-agentpath:$JXPerf_HOME/bin/wallclock/wallClockProfile.so=start,event=wall,interval=5ms,file=profile.html
+PROGRAM="$1"
+
+"$JAVA" "$INSTRUMENT" -jar $JXPerf_HOME/benchmark/dacapo.jar -s large lusearch
