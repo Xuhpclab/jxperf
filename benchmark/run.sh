6b4193945a895b9d48fc6a3d794654954c026924
bolunli11 <bli35@ncsu.edu>
2021-11-28 16:54:43 -0500

    benchmark

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,8 +1,8 @@
 # CPU Cycles
-# LD_PRELOAD=$JXPerf_HOME/build/libpreload.so
-# java \
-#     -agentpath:$JXPerf_HOME/build/libagent.so=Generic::CYCLES:precise=2@100000 \
-#     -jar ./dacapo.jar lusearch
+LD_PRELOAD=$JXPerf_HOME/build/libpreload.so
+java \
+    -agentpath:$JXPerf_HOME/build/libagent.so=Generic::CYCLES:precise=2@1000000 \
+    -jar ./dacapo.jar lusearch
 
 # dead store
 # LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
@@ -13,10 +13,10 @@
 # java -javaagent:$JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar -agentpath:$JXPerf_HOME/build/libagent.so=DataCentric::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000 -jar ./dacapo.jar -s large lusearch
 
 # Heap usage
-LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
-java \
-    -agentpath:$JXPerf_HOME/build/libagent.so=Heap \
-    -jar ./dacapo.jar -s large lusearch
+# LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
+# java \
+#     -agentpath:$JXPerf_HOME/build/libagent.so=Heap \
+#     -jar ./dacapo.jar -s large lusearch
 
 # Offline process -- VS Code GUI
 # $JXPerf_HOME/script/process_raw_data_to_vscode.py $JXPerf_HOME/benchmark/lucene
