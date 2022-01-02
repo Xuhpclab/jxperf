c3cb804c380a3621140a9b2018a3649e3dd89d77
bolunli11 <bli35@ncsu.edu>
2022-01-02 15:40:37 -0500

    update run.sh

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,25 +1,15 @@
-# CPU Cycles
+# 1.CPU Cycles
+# LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
+# java \
+#     -agentpath:$JXPerf_HOME/build/libagent.so=Generic::CYCLES:precise=2@1000000 \
+#     -jar ./dacapo.jar lusearch
+
+# 2.Data-centric
 LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
-java \
-    -agentpath:$JXPerf_HOME/build/libagent.so=Generic::CYCLES:precise=2@1000000 \
-    -jar ./dacapo.jar lusearch
+java -javaagent:$JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar -agentpath:$JXPerf_HOME/build/libagent.so=DataCentric::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000 -jar ./dacapo.jar -s large lusearch
 
-# dead store
-# LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
-# java -agentpath:$JXPerf_HOME/build/libagent.so=SilentStore::MEM_UOPS_RETIRED:ALL_STORES:precise=2@100000 -jar ./dacapo.jar lusearch
-
-# data centric
-# LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
-# java -javaagent:$JXPerf_HOME/thirdparty/allocation-instrumenter/target/java-allocation-instrumenter-HEAD-SNAPSHOT.jar -agentpath:$JXPerf_HOME/build/libagent.so=DataCentric::MEM_LOAD_UOPS_RETIRED:L1_MISS:precise=2@10000 -jar ./dacapo.jar -s large lusearch
-
-# Heap usage
+# 3.Heap usage
 # LD_PRELOAD=$JXPerf_HOME/build/preload/libpreload.so
 # java \
 #     -agentpath:$JXPerf_HOME/build/libagent.so=Heap \
 #     -jar ./dacapo.jar -s large lusearch
-
-# Offline process -- VS Code GUI
-$JXPerf_HOME/script/process_raw_data_to_vscode.py $JXPerf_HOME/benchmark/lucene
-
-# View
-# code ./lucene
